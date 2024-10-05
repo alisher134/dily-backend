983a32229932e88215997535966bd42f7f83de76
alisher134 <alisherrakhmanov134@gmail.com>
2024-10-05 16:27:33 +0500

    feat: add basic configuration of the project

---
+++ prisma/migrations/20241005112007_add_user_model/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "EnumUserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "avatarPath" TEXT NOT NULL DEFAULT '/uploads/default-avatar.png',
+    "role" "EnumUserRole" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const config = app.get(ConfigService);
+
+	app.setGlobalPrefix('api/v1');
+	app.enableCors({
+		origin: config.get('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposedHeaders: ['set-cookie']
+	});
+
+	await app.listen(config.get('PORT'));
 }
 bootstrap();
